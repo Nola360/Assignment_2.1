57f0118aab64456ae25a38462f3c2c313bedd8c1
Nola360 <40280292+Nola360@users.noreply.github.com>
2022-07-18 16:57:03 -0400

    Add files via upload

---
+++ a_daramola_2.1.py
@@ -0,0 +1,8 @@
+#Akinola Daramola Jr
+#Programming Exercise 2.1
+#Due Date: 6/12/2022
+
+print("Name: Akinola Daramola Jr")
+print("Address: 81 Center St Woonsocket, RI 02895")
+print("Phone Number: 401-744-8857")
+print('Major: Computer Programming')
